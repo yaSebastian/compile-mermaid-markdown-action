7552b8ea0269e294feb65857980fdb2a9526d45b
Sebastian Werner <dev@sebastianwerner.net>
2021-01-27 01:03:33 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -80,10 +80,10 @@ function is_path_markdown {
 # $2 - the output location
 function c_mermaid {
   # longer version: if [[ -n $files ]]; then files="$files\n"; fi
-  [[ -n $out_files_all ]] && out_files_all='$out_files_all\n'
+  [[ -n $out_files_all ]] && out_files_all="$out_files_all\n"
   out_files_all="$out_files_all${2}"
   if [[ ! -f ${2} ]]; then
-    [[ -n $out_files_new ]] && out_files_new='$out_files_new\n'
+    [[ -n $out_files_new ]] && out_files_new="$out_files_new\n"
     out_files_new="$out_files_new${2}"
   fi
 
